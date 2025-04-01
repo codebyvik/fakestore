d3d55cf9e3c48883cd86a8f56f7fc6fa7e88a4d6
vikas <vikasvickycoorg@gmail.com>
2025-04-01 19:48:36 +0530

    integrated redux

---
+++ src/redux/product-slice.ts
@@ -0,0 +1,61 @@
+import { ProductStateType } from "@/types/products/product-state.types";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialProductState: ProductStateType = {
+  productList: {
+    list: null,
+    isFetching: false,
+    isSuccess: false,
+    error: null,
+  },
+  productDetails: {
+    details: null,
+    isFetching: false,
+    isSuccess: false,
+    error: null,
+  },
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState: initialProductState,
+  reducers: {
+    // Fetching list of products
+    fetchAllProducts: (state) => {
+      state.productList.isFetching = true;
+    },
+    fetchAllProductsSuccess: (state, action) => {
+      state.productList.isFetching = false;
+      state.productList.list = action.payload;
+    },
+    fetchAllProductsError: (state, action) => {
+      state.productList.isFetching = false;
+      state.productList.isSuccess = false;
+      state.productList.error = action.payload;
+    },
+    // Fetching products Details
+    fetchProductDetails: (state) => {
+      state.productDetails.isFetching = true;
+    },
+    fetchProductDetailsSuccess: (state, action) => {
+      state.productDetails.isFetching = false;
+      state.productDetails.details = action.payload;
+    },
+    fetchProductDetailsError: (state, action) => {
+      state.productDetails.isFetching = false;
+      state.productDetails.isSuccess = false;
+      state.productList.error = action.payload;
+    },
+  },
+});
+
+export const {
+  fetchAllProducts,
+  fetchAllProductsSuccess,
+  fetchAllProductsError,
+  fetchProductDetails,
+  fetchProductDetailsError,
+  fetchProductDetailsSuccess,
+} = productSlice.actions;
+
+export default productSlice.reducer;
---
+++ src/types/products/product-state.types.ts
@@ -0,0 +1,23 @@
+export interface ProductStateType {
+  productList: {
+    list: ProductDetailsType[] | null;
+    isFetching: boolean;
+    isSuccess: boolean;
+    error: any;
+  };
+  productDetails: {
+    details: ProductDetailsType | null;
+    isFetching: boolean;
+    isSuccess: boolean;
+    error: any;
+  };
+}
+
+export interface ProductDetailsType {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
