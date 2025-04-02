469fad8b0e787bb1217f514470f69f5d3004078d
vikas <vikasvickycoorg@gmail.com>
2025-04-02 15:04:35 +0530

    changes to hashrouter to fix github pages issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.ts";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 
@@ -11,9 +11,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </StrictMode>
